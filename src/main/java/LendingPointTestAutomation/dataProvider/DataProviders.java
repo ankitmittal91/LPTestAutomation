b7a876b3cce6a95d19722fafbbbf5e5b8a125cad
Ankit Mittal <ankit.mittal@trantorchd.com>
2024-09-12 17:57:37 +0530

    update data provider to use arraylist and maps

--- src/main/java/LendingPointTestAutomation/dataProvider/DataProviders.java
+++ src/main/java/LendingPointTestAutomation/dataProvider/DataProviders.java
@@ -1,4 +1,10 @@
 package LendingPointTestAutomation.dataProvider;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import org.testng.annotations.DataProvider;
 import LendingPointTestAutomation.utility.NewExcelLibrary;
 
@@ -7,22 +13,33 @@ public class DataProviders {
 	NewExcelLibrary file = new NewExcelLibrary();
 	
 	@DataProvider(name = "userData")
-	public Object[][] getCredentials() {
+	public Iterator<Object[]> getCredentials() {
 		// Totals rows count
 		int rows = file.getRowCount("UserData");
 		// Total Columns
 		int column = file.getColumnCount("UserData");
 		int actRows = rows - 1;
-
-		Object[][] data = new Object[actRows][column];
-
-		for (int i = 0; i < actRows; i++) { 
-			for (int j = 0; j < column; j++) { 
-				data[i][j] = file.getCellData("UserData", j, i + 2);
+		List<Map<String,String>> listOfMap = new ArrayList<>();
+		ArrayList<Object[]> list = new ArrayList<>();
+		Map<String,String> map = new HashMap<>();
+		for(int i=0; i<actRows; i++) {
+			for(int j=0; j<column; j++) {
+				map.put(file.getCellData("UserData", j, 1), file.getCellData("UserData", j, i+2));
 			}
+			listOfMap.add(map);
+			map = new HashMap<>();
 		}
-		return data;
-	}
-	
+		
+		for(Map<String,String> mapping : listOfMap) {
+			list.add(new Object[] {mapping});
+		}
 
+		/*
+		 * Object[][] data = new Object[actRows][column];
+		 * for (int i = 0; i < actRows;i++) { 
+		 * 		for (int j = 0; j < column; j++) { 
+		 * 			data[i][j] = file.getCellData("UserData", j, i + 2); } }
+		 */
+		return list.iterator();
+	}
 }
--- src/test/java/LendingPointTestAutomation/applyUITests/ApplyPageTest.java
+++ src/test/java/LendingPointTestAutomation/applyUITests/ApplyPageTest.java
@@ -1,5 +1,7 @@
 package LendingPointTestAutomation.applyUITests;
 
+import java.util.Map;
+
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -36,8 +38,49 @@ connectDepositAccountPage connectDepositAccountPage;
 		}
 	}
 	
-	@Test (dataProvider = "userData", dataProviderClass = DataProviders.class , groups = {"Sanity","Smoke"})
-	public void newApplyFundingFlow(String firstName, String lastName, String loanAmount, String mobilePhone, String email, String address, String city, String state, String zip, String annualIncome, String incomeType, String companyName, String dob, String ssn) throws InterruptedException {
+	@Test (dataProvider = "userData", dataProviderClass = DataProviders.class , groups = {"Sanity","Smoke"}, priority =1)
+	public void newApplyFundingFlow(Map<String,String> map) throws InterruptedException {
+			Log.startTestCase("User starts entering Personal Information");
+		applyPage.enterFirstName(map.get("First Name"));
+		applyPage.enterLastName(map.get("Last Name"));
+		applyPage.enterLoanAmount(map.get("Loan Amount"));
+		applyPage.selectLoanPurpose();
+		applyDetailsContactInfoPage = applyPage.clickContinueButton();
+			Action.fluentWait(applyDetailsContactInfoPage.mobilePhoneTextBox, 10);
+		applyDetailsContactInfoPage.enterMobilePhone("7543074253");
+		applyDetailsContactInfoPage.enterEmail(map.get("Email Address"));
+		verifyPhone = applyDetailsContactInfoPage.clickContinueButton();
+			Action.fluentWait(verifyPhone.codeTextBox1, 10);
+		verifyPhone.enterVerificationCode("123456");
+		incomeAddressFillPage = verifyPhone.clickVerifyButton();
+			Action.fluentWait(incomeAddressFillPage.addressTextBox, 20);
+		incomeAddressFillPage.enterAddress(map.get("Address"));
+		incomeAddressFillPage.enterCity(map.get("City"));
+		incomeAddressFillPage.selectState(map.get("State"));
+		incomeAddressFillPage.enterZip(map.get("Zip"));
+		yourIncomePage = incomeAddressFillPage.clickContinuetoIncomeButton();
+			Action.fluentWait(yourIncomePage.annualIncomeTextBox, 10);
+		yourIncomePage.enterAnnualIncome(map.get("Annual Income"));
+		yourIncomePage.selectIncomeType(map.get("Income Type"));
+			Action.fluentWait(yourIncomePage.companyNameTextBox, 10);
+		yourIncomePage.enterCompanyName(map.get("Company Name"));
+		aboutYouDOBSSNPage = yourIncomePage.clickContinueToAboutYouButton();
+			Action.fluentWait(aboutYouDOBSSNPage.dobTextBox, 10);
+		aboutYouDOBSSNPage.enterDOB(map.get("DOB"));
+		aboutYouDOBSSNPage.enterSSN(map.get("SSN"));
+		selectOfferPage = aboutYouDOBSSNPage.clickSeeOptionsButton();
+			DriverFactory.getInstance().getDriver().manage().timeouts().wait(30000);
+			Action.fluentWait(selectOfferPage.congratulationsText, 60);
+		offerConfirmPage = selectOfferPage.clickConfirmOfferButton();
+			Action.fluentWait(offerConfirmPage.offerConfirmedText, 60);
+		connectDepositAccountPage = offerConfirmPage.clickLetsGoButton();
+			Action.fluentWait(connectDepositAccountPage.connectDepositAccountText, 60);
+		connectDepositAccountPage.clickConnectBankButton();
+		Log.endTestCase("User finished entering Personal Information");
+	}
+	
+	@Test (dataProvider = "userData", dataProviderClass = DataProviders.class , groups = {"Sanity","Smoke"}, priority =2)
+	public void newApplyFundingFlow1(String firstName, String lastName, String loanAmount, String mobilePhone, String email, String address, String city, String state, String zip, String annualIncome, String incomeType, String companyName, String dob, String ssn) throws InterruptedException {
 			Log.startTestCase("User starts entering Personal Information");
 		applyPage.enterFirstName(firstName);
 		applyPage.enterLastName(lastName);
